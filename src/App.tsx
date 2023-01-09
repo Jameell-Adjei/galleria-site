4225e76209ee6124cc7da58bba6b0ba1f427f991
JayAlmighty00 <jameelladjei@gmail.com>
2023-01-09 16:06:23 -0500

    refactor: set up color variables and font styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
+import "./styles/app.scss"
 
 function App() {
 
 
   return (
     <div className="App">
-
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolorem, consequatur laboriosam quia tempore excepturi eveniet sapiente optio ex, necessitatibus beatae iure dolor. Ad ullam quas est veniam, cumque nemo?</p>
     </div>
   )
 }
