10db5c399b24b8c8e178c690821d99ebd8d12120
JayAlmighty00 <jameelladjei@gmail.com>
2023-02-17 17:29:10 -0500

    feat: add react router to the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import "./styles/app.scss"
-import './Navbar'
-import Navbar from "./Navbar"
-import ImageContainer from "./ImageContainer"
+import Navbar from "./Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/app.scss";
+import HomePage from "./HomePage";
+import { DetailsPageProvider } from "./context/DetailsPageContext";
+import DetailsPage from "./DetailsPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/slideshow",
+    element: <DetailsPage />,
+  },
+]);
+
 function App() {
-
-
   return (
-    <div className="App">
-      <Navbar/>
-      <ImageContainer/>
-    </div>
-  )
+    <>
+      <DetailsPageProvider>
+        <Navbar/>
+        <div className="App">
+          <RouterProvider router={router} />
+        </div>
+      </DetailsPageProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
