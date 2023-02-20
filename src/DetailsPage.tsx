2cc4f40a8ff9e1bd89de21ea46248e75940ad4cb
JayAlmighty00 <jameelladjei@gmail.com>
2023-02-20 13:24:47 -0500

    feat: add styling for the painting and painting info to the Details Page

--- src/DetailsPage.tsx
+++ src/DetailsPage.tsx
@@ -12,10 +12,17 @@ const DetailsPage = () => {
     },[currentIndex])
   return (
     <>
-      <main>
-        <picture>
-          <img src={currentSlide.images.hero.small} alt="" srcSet="" />
-        </picture>
+      <main className="detailsPage">
+        <section>
+          <picture className="detailsPage__image-container">
+            <img id='detailsPage__image' src={currentSlide.images.hero.small} alt="" srcSet="" />
+          </picture>
+          <div className="detailsPage__painting-info">
+            <h2 className="detailsPage__painting-name">{currentSlide.name}</h2> 
+            <p id="detailsPage__artist-name">{currentSlide.artist.name}</p>          
+          </div>
+        </section>
+
       
       </main>    
     </>
