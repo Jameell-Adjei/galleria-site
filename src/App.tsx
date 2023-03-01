5023f07d32ddef8e2a05de3814f60b6653d772d9
JayAlmighty00 <jameelladjei@gmail.com>
2023-03-01 16:14:14 -0500

    fix: navbar renders on all routes properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import Navbar from "./Navbar";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/app.scss";
-import HomePage from "./HomePage";
 import { DetailsPageProvider } from "./context/DetailsPageContext";
-import DetailsPage from "./DetailsPage";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/slideshow",
-    element: <DetailsPage />,
-  },
-]);
+import { Outlet } from "react-router-dom";
+
+
 
 function App() {
   return (
     <>
       <DetailsPageProvider>
-        <Navbar/>
         <div className="App">
-          <RouterProvider router={router} />
+          <Navbar/>
+          <Outlet/>
         </div>
       </DetailsPageProvider>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import "./styles/app.scss";
+import DetailsPage from "./DetailsPage";
+import ImageContainer from "./ImageContainer";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children :[
+      {
+        path: "/",
+        element: <ImageContainer/>
+      },
+      {
+        path: "/slideshow",
+        element: <DetailsPage />,
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
