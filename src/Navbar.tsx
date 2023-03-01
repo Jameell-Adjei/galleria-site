a33296d7753af862e3468f49287a4b17bea9a36c
JayAlmighty00 <jameelladjei@gmail.com>
2023-03-01 16:26:44 -0500

    feat: navbar logo links back to homepage on click

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,10 +1,11 @@
 import Logo from './assets/shared/logo.svg';
-
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className='navbar'>
+     <Link to={'/'}> 
       <img src={Logo} alt="The logo for the Galleria Slideshow Website" className='navbar__logo'/>
+     </Link>
       <span className='navbar__slideshow--start'>Enter Slideshow</span>
     </nav>
   )
