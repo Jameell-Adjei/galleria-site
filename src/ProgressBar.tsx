e054116b3592884df80e1a7903c387018586444b
JayAlmighty00 <jameelladjei@gmail.com>
2023-03-03 17:45:56 -0500

    feat: create the progress lines for each slide

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,17 +1,24 @@
-import { useCurrentSlide } from "./context/DetailsPageContext";
+import { useCurrentSlide, useDetails } from "./context/DetailsPageContext";
 import { ReactComponent as BackButton } from  "./assets/shared/icon-back-button.svg";
 import { ReactComponent as NextButton } from  "./assets/shared/icon-next-button.svg";
 
 const ProgressBar = () => {
   const { currentSlide , updateIndex } = useCurrentSlide();
   let { currentIndex } = useCurrentSlide();
+  const { state } = useDetails();
 
+  let amountSeen = (currentIndex + 1)/state.slides.length;
+  
   return (
     <section id="progress-bar">
+      <div className="progress"style={{ width: `100%`, height: "1px", background:"#E5E5E5"}}/>
+      <div className="progress"style={{ width: `${amountSeen * 100}%`, height: "1px", background:"black"}}/>
+
       <div>
         <h2 id="progress-bar__painting">{currentSlide.name}</h2>
         <p id="progress-bar__artist">{currentSlide.artist.name}</p>
       </div>
+      
       <div className="progress-bar__button-container">
         <BackButton onClick={() =>{
             updateIndex(currentIndex -= 1)            
