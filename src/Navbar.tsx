293077995627aa0a5689a9eb9cb9a968918b95fb
JayAlmighty00 <jameelladjei@gmail.com>
2023-03-12 15:18:01 -0400

    feat: add slideshow controls to the navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,12 +1,14 @@
 import Logo from './assets/shared/logo.svg';
 import { Link } from 'react-router-dom';
+import { useSlideShow } from './context/DetailsPageContext';
 const Navbar = () => {
+  const { slideShowID, startSlideshow, stopSlideshow } = useSlideShow();
   return (
     <nav className='navbar'>
      <Link to={'/'}> 
-      <img src={Logo} alt="The logo for the Galleria Slideshow Website" className='navbar__logo'/>
+      <img src={Logo} onClick={stopSlideshow} alt="The logo for the Galleria Slideshow Website" className='navbar__logo'/>
      </Link>
-      <span className='navbar__slideshow--start'>Enter Slideshow</span>
+      <span onClick={startSlideshow} className='navbar__slideshow--start'>{slideShowID ? "Stop Slideshow" : "Enter Slideshow"}</span>
     </nav>
   )
 }
