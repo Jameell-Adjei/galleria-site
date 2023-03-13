a6fdeb67e7344d17376ea8facf5b371894166162
JayAlmighty00 <jameelladjei@gmail.com>
2023-03-13 17:09:16 -0400

    feat: add styling to the LightBox compnonent

--- src/LightBox.tsx
+++ src/LightBox.tsx
@@ -7,10 +7,12 @@ const LightBox = () => {
   const { open , changeLightBox } = useLightBox();
 
   return (
-      <div className="lightBox" style={{ visibility : `${open ? "visible" : "hidden" }`}} onClick={changeLightBox}>
-        <span onClick={changeLightBox}>C L O S E</span>
-        <img src={currentSlide.images.thumbnail} alt="" srcSet="" />
-      </div>   
+      <section className="lightBox" style={{ visibility : `${open ? "visible" : "hidden" }` , display : `${open ? "flex" : "none"}`}} onClick={changeLightBox}>
+        <div style={{position: "relative"}}>
+          <span className="lightBox-text">CLOSE</span>
+          <img src={currentSlide.images.thumbnail} alt="" srcSet="" />
+        </div> 
+      </section>   
   )
 }
 
