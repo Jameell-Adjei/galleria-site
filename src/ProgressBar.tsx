d3289d62d5549e2149d57890f64ea26f391d1154
JayAlmighty00 <jameelladjei@gmail.com>
2023-03-24 15:40:15 -0400

    Revert "feat: add disabled and hover effects to progress bar buttons"
    This reverts commit 284ebba12944cee78e0deecbff91383707f7d6a1.

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,7 +1,6 @@
 import { useCurrentSlide, useDetails } from "./context/DetailsPageContext";
 import { ReactComponent as BackButton } from  "./assets/shared/icon-back-button.svg";
 import { ReactComponent as NextButton } from  "./assets/shared/icon-next-button.svg";
-import { useState , useEffect } from "react";
 
 const ProgressBar = () => {
   const { currentSlide , updateIndex } = useCurrentSlide();
@@ -9,23 +8,6 @@ const ProgressBar = () => {
   const { state } = useDetails();
 
   let amountSeen = (currentIndex + 1)/state.slides.length;
-  const [disabled , setDisabled] = useState<boolean>(false)
-  const [rightDisabled , setRightDisabled] = useState<boolean>(false)
-
-  useEffect(() => {
-    if(state.currentIndex === 0){
-      setDisabled(true)   
-    } else if(state.currentIndex === state.slides.length - 1){
-      setRightDisabled(true)
-    }
-
-    // Clean up function
-    return () => {
-      setDisabled(false)
-      setRightDisabled(false)
-    }
-  }, [state.currentIndex])
-  
   
   return (
     <section id="progress-bar">
@@ -38,10 +20,10 @@ const ProgressBar = () => {
       </div>
       
       <div className="progress-bar__button-container">
-        <BackButton className={`${disabled ? "disabled" : "progress-bar--button"}`} onClick={() =>{
+        <BackButton onClick={() =>{
             updateIndex(currentIndex -= 1)            
         }}/>
-        <NextButton className={`${rightDisabled ? "disabled" : "progress-bar--button"}`} onClick={()=>{
+        <NextButton onClick={()=>{
             updateIndex(currentIndex += 1)
         }}/>
       </div>
