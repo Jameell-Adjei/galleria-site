0252f1cd2bbbe016f6b8b2166db66a6dc81c30ff
JayAlmighty00 <jameelladjei@gmail.com>
2023-04-05 15:59:21 -0400

    fix: add back the progress bar code that was removed

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,6 +1,7 @@
 import { useCurrentSlide, useDetails } from "./context/DetailsPageContext";
 import { ReactComponent as BackButton } from  "./assets/shared/icon-back-button.svg";
 import { ReactComponent as NextButton } from  "./assets/shared/icon-next-button.svg";
+import { useEffect, useState } from "react";
 
 const ProgressBar = () => {
   const { currentSlide , updateIndex } = useCurrentSlide();
@@ -8,6 +9,22 @@ const ProgressBar = () => {
   const { state } = useDetails();
 
   let amountSeen = (currentIndex + 1)/state.slides.length;
+  const [disabled ,setDisabled] = useState<boolean>(false);
+  const [rightDisabled ,setRightDisabled] = useState<boolean>(false);
+
+  useEffect(()=>{
+    if(state.currentIndex === 0){
+      setDisabled(true);
+    } else if(state.currentIndex === state.slides.length - 1){
+      setRightDisabled(true);
+    }
+
+    // Clean up function
+    return ()=>{
+      setDisabled(false);
+      setRightDisabled(false);
+    }
+  },[state.currentIndex])
   
   return (
     <section id="progress-bar">
@@ -20,10 +37,10 @@ const ProgressBar = () => {
       </div>
       
       <div className="progress-bar__button-container">
-        <BackButton onClick={() =>{
+      <BackButton className={`${disabled ? "disabled" : "progress-bar--button"}`} onClick={() =>{
             updateIndex(currentIndex -= 1)            
         }}/>
-        <NextButton onClick={()=>{
+        <NextButton className={`${rightDisabled ? "disabled" : "progress-bar--button"}`} onClick={()=>{
             updateIndex(currentIndex += 1)
         }}/>
       </div>
