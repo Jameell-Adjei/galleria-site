ca4e5a8c9dbd2b8a71a8a1df62cdaa695f82da16
JayAlmighty00 <jameelladjei@gmail.com>
2023-04-22 22:23:47 -0400

    feat: add hover effects to multiple files and adjust styles for the layout

--- src/LightBox.tsx
+++ src/LightBox.tsx
@@ -8,13 +8,13 @@ const LightBox = () => {
 
   return (
       <section className="lightBox" style={{ visibility : `${open ? "visible" : "hidden" }` , display : `${open ? "flex" : "none"}`}} onClick={changeLightBox}>
-        <div style={{position: "relative"}}>
+        <div>
           <span className="lightBox-text">CLOSE</span>
           <picture>
             <source media="(min-width: 768px)" srcSet={`${currentSlide.images.gallery}`}/> 
             <img src={currentSlide.images.thumbnail} alt="" srcSet="" /> 
           </picture>
-          
+
         </div> 
       </section>   
   )
