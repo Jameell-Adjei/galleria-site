03c894772706ffe320845829b8da9e81625f76b2
JayAlmighty00 <jameelladjei@gmail.com>
2023-04-26 11:09:37 -0400

    feat: add navbar functionality to start slideshows from other pages

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,14 +1,23 @@
 import Logo from './assets/shared/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate } from 'react-router-dom';
 import { useSlideShow } from './context/DetailsPageContext';
 const Navbar = () => {
-  const { slideShowID, startSlideshow, stopSlideshow } = useSlideShow();
+  const { slideShowID, startSlideshow, stopSlideshow, resetCurrentIndex } = useSlideShow();
+  const navigate = useNavigate();
+  const handleClick =() =>{
+    if(slideShowID === 0){
+      navigate("/slideshow");
+      startSlideshow();
+    } else {
+      stopSlideshow();
+    }
+  }
   return (
     <nav className='navbar'>
      <Link to={'/'}> 
-      <img src={Logo} onClick={stopSlideshow} alt="The logo for the Galleria Slideshow Website" className='navbar__logo'/>
+      <img src={Logo} onClick={()=>{resetCurrentIndex(); stopSlideshow()}} alt="The logo for the Galleria Slideshow Website" className='navbar__logo'/>
      </Link>
-      <span onClick={startSlideshow} className='navbar__slideshow--start'>{slideShowID ? "Stop Slideshow" : "Enter Slideshow"}</span>
+      <span onClick={handleClick} className='navbar__slideshow--start'>{slideShowID ? "Stop Slideshow" : "Enter Slideshow"}</span>
     </nav>
   )
 }
