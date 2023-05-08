34e14a1a310ae0f8b9f5a01bb5f65144c01d24df
JayAlmighty00 <jameelladjei@gmail.com>
2023-05-08 16:57:00 -0400

    fix: added buttons to ProgressBar to correct onClick not working

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,6 +1,6 @@
 import { useCurrentSlide, useDetails } from "./context/DetailsPageContext";
-import { ReactComponent as BackButton } from  "./assets/shared/icon-back-button.svg";
-import { ReactComponent as NextButton } from  "./assets/shared/icon-next-button.svg";
+import { ReactComponent as BackButton } from "./assets/shared/icon-back-button.svg";
+import { ReactComponent as NextButton } from "./assets/shared/icon-next-button.svg";
 import { useEffect, useState } from "react";
 
 const ProgressBar = () => {
@@ -8,41 +8,63 @@ const ProgressBar = () => {
   const { currentIndex } = useCurrentSlide();
   const { state } = useDetails();
 
-  let amountSeen = (currentIndex + 1)/state.slides.length;
-  const [disabled ,setDisabled] = useState<boolean>(false);
-  const [rightDisabled ,setRightDisabled] = useState<boolean>(false);
+  let amountSeen = (currentIndex + 1) / state.slides.length;
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const [rightDisabled, setRightDisabled] = useState<boolean>(false);
 
-  useEffect(()=>{
-    if(currentIndex === 0){
+  useEffect(() => {
+    if (currentIndex === 0) {
       setDisabled(true);
-    } else if(currentIndex === state.slides.length - 1){
+    } else if (currentIndex === state.slides.length - 1) {
       setRightDisabled(true);
     }
 
     // Clean up function
-    return ()=>{
+    return () => {
       setDisabled(false);
       setRightDisabled(false);
-    }
-  },[currentIndex])
-  
+    };
+  }, [currentIndex]);
+
   return (
     <section id="progress-bar">
-      <div className="progress"style={{ width: `100%`, height: "1px", background:"#E5E5E5"}}/>
-      <div className="progress"style={{ width: `${amountSeen * 100}%`, height: "1px", background:"black"}}/>
+      <div
+        className="progress"
+        style={{ width: `100%`, height: "1px", background: "#E5E5E5" }}
+      />
+      <div
+        className="progress"
+        style={{
+          width: `${amountSeen * 100}%`,
+          height: "1px",
+          background: "black",
+        }}
+      />
 
       <div>
         <h2 id="progress-bar__painting">{currentSlide.name}</h2>
         <p id="progress-bar__artist">{currentSlide.artist.name}</p>
       </div>
-      
+
       <div className="progress-bar__button-container">
-      <BackButton className={`${disabled ? "disabled" : "progress-bar--button"}`} onClick={() =>{  
-          decrementCurrentIndex();       
-        }}/>
-        <NextButton className={`${rightDisabled ? "disabled" : "progress-bar--button"}`} onClick={()=>{
-          incrementCurrentIndex();
-        }}/>
+        <button
+          onClick={decrementCurrentIndex}
+          className={`${disabled ? "disabled" : ""}`}
+        >
+          <BackButton
+            className={`${disabled ? "" : "progress-bar--button"} `}
+          />
+        </button>
+        <button
+          onClick={incrementCurrentIndex}
+          className={`${rightDisabled ? "disabled" : ""}`}
+        >
+          <NextButton
+            className={`${
+              rightDisabled ? "" : "progress-bar--button"
+            } `}
+          />
+        </button>
       </div>
     </section>
   );
